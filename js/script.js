a35b02f23c525a1502d29670b482cd908183add3
kirandash <thekirankumardash@gmail.com>
2016-07-09 23:26:09 +0530

    Editing Runs

--- js/script.js
+++ js/script.js
@@ -3,36 +3,43 @@ $(document).one('pageinit', function(){
 	//Display runs
 	showRuns();
 	
-	//Add Handler
+	// Add Handler
 	$('#submitAdd').on('tap', addRun);
 
+	// Edit Handler
+	$('#submitEdit').on('tap', editRun);
+
+	// Set Current Handler
+	$('#stats').on('tap','#editLink', setCurrent);
+		
 	/*
 	 *  Show all runs on a homepage
 	 */
 	 
 	 function showRuns(){
-		//get runs object
+		// get runs object
 		var runs =  getRunsObject();
 		
-		//Check if empty
+		// Check if empty
 		if(runs != '' && runs != null){
 			for(var i=0; i<runs.length; i++){
-				$('#stats').append('<li class="ui-body-inherit ui-li-static"><strong>Date:</strong>'+runs[i]["date"]+' <br><strong>Distance:</strong>'+runs[i]["miles"]+'m<div class="controls"><a href="#edit">Edit</a> | <a href="#">Delete</a></div></li>');
+				$('#stats').append('<li class="ui-body-inherit ui-li-static"><strong>Date:</strong>'+runs[i]["date"]+' <br><strong>Distance:</strong>'+runs[i]["miles"]+'m<div class="controls"><a href="#edit" id="editLink" data-miles="'+runs[i]["miles"]+'" data-date="'+runs[i]["date"]+'">Edit</a> | <a href="#">Delete</a></div></li>');
 			}
 			$('#home').bind('pageinit', function(){
 				$('#stats').listview('refresh');
 			});
 		}
 	 }
+	 
 	/*
 	 *  Add a Run
 	 */
 	 function addRun() {
-		 //Get form values
+		 // Get form values
 		 var miles = $('#addMiles').val();
 		 var date = $('#addDate').val();
 		 
-		 //Create 'run' object
+		 // Create 'run' object
 		 var run = {
 			date: date,
 			miles: parseFloat(miles)
@@ -40,12 +47,12 @@ $(document).one('pageinit', function(){
 		 
 		 var runs = getRunsObject();
 		 
-		 //Add run to runs array
+		 // Add run to runs array
 		 runs.push(run);
 		 
 		 alert('Run Added');
 		 
-		 //Set stringified object to localstorage
+		 // Set stringified object to localstorage
 		 localStorage.setItem('runs', JSON.stringify(runs));
 		 
 		 //Redirect
@@ -53,23 +60,78 @@ $(document).one('pageinit', function(){
 		 
 		 return false;
 	 }
-	 
+
+	/*
+	 *  Edit Run
+	 */
+	 function editRun() {
+		 // Get current data
+		 var currentMiles = localStorage.getItem('currentMiles');
+		 var currentDate = localStorage.getItem('currentDate');
+
+		 var runs = getRunsObject();
+		 
+		 // Loop through runs (To remove the existing data and make it a string back)
+		 for(var i=0; i<runs.length; i++){
+			if(runs[i].miles == currentMiles && runs[i].date == currentDate){
+				runs.splice(i,1);
+			}
+			localStorage.setItem('runs',JSON.stringify(runs));
+		 }
+		 		 
+		 // Get form values
+		 var miles = $('#editMiles').val();
+		 var date = $('#editDate').val();
+		 
+		 // Create 'run' object
+		 var update_run = {
+			date: date,
+			miles: parseFloat(miles)
+		 };
+		 		 
+		 // Add run to runs array
+		 runs.push(update_run);
+		 
+		 alert('Run Updated');
+		 
+		 // Set stringified object to localstorage
+		 localStorage.setItem('runs', JSON.stringify(runs));
+		 
+		 //Redirect
+		 window.location.href="index.html";
+		 
+		 return false;
+	 }
+	 	 
 	/*
 	 *  Get the runs object
 	 */
 	 function getRunsObject() {
 		 // Set runs array
 		 var runs = new Array();
-		 //Get current runs from localStorage
+		 // Get current runs from localStorage
 		 var currentRuns = localStorage.getItem('runs');
 		 
-		 //Check localStorage
+		 // Check localStorage
 		 if(currentRuns != null){
-			 //Set to runs
+			 // Set to runs
 			 var runs = JSON.parse(currentRuns);
 		 }
 		 
-		 //Return runs object
+		 // Return runs object
 		 return runs.sort(function(a,b){return new Date(b.date) - new Date(a.date)});			
 	 }	 
+	 
+	/*
+	 *  Set the current clicked miles and date
+	 */
+	 function setCurrent() {
+		// Set ls items
+		localStorage.setItem('currentMiles',$(this).data('miles'));
+		localStorage.setItem('currentDate',$(this).data('date'));		
+		
+		// Insert form fields
+		$('#editMiles').val(localStorage.getItem('currentMiles'));
+		$('#editDate').val(localStorage.getItem('currentDate'));
+	 }		 
 });
