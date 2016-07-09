e7ad9709777c27eeb625a0bfd30e00588bfb5048
kirandash <thekirankumardash@gmail.com>
2016-07-09 18:25:46 +0530

    Adding Runs to LocalStorage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,54 @@
 // JavaScript Document
+$(document).one('pageinit', function(){
+	//Add Handler
+	$('#submitAdd').on('tap', addRun);
+	
+	/*
+	 *  Add a Run
+	 */
+	 function addRun() {
+		 //Get form values
+		 var miles = $('#addMiles').val();
+		 var date = $('#addDate').val();
+		 
+		 //Create 'run' object
+		 var run = {
+			date: date,
+			miles: parseFloat(miles)
+		 };
+		 
+		 var runs = getRunsObject();
+		 
+		 //Add run to runs array
+		 runs.push(run);
+		 
+		 alert('Run Added');
+		 
+		 //Set stringified object to localstorage
+		 localStorage.setItem('runs', JSON.stringify(runs));
+		 
+		 //Redirect
+		 window.location.href="index.html";
+		 
+		 return false;
+	 }
+	 
+	/*
+	 *  Get the runs object
+	 */
+	 function getRunsObject() {
+		 // Set runs array
+		 var runs = new Array();
+		 //Get current runs from localStorage
+		 var currentRuns = localStorage.getItem('runs');
+		 
+		 //Check localStorage
+		 if(currentRuns != null){
+			 //Set to runs
+			 var runs = JSON.parse(currentRuns);
+		 }
+		 
+		 //Return runs object
+		 return runs.sort(function(a,b){return new Date(b.date) - new Date(a.date)});			
+	 }	 
+});
