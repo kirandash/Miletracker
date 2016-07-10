ca10f0a4a1f3fdec37b74318c2e4e9acfa4c2e64
kirandash <thekirankumardash@gmail.com>
2016-07-10 17:14:49 +0530

    Deleting Runs from localStorage

--- js/script.js
+++ js/script.js
@@ -8,9 +8,15 @@ $(document).one('pageinit', function(){
 
 	// Edit Handler
 	$('#submitEdit').on('tap', editRun);
+	
+	// Delete Handler
+	$('#stats').on('tap','#deleteLink', deleteRun);
 
 	// Set Current Handler
 	$('#stats').on('tap','#editLink', setCurrent);
+	
+	// Clear Handler
+	$('#clearRuns').on('tap', clearRuns);
 		
 	/*
 	 *  Show all runs on a homepage
@@ -23,11 +29,13 @@ $(document).one('pageinit', function(){
 		// Check if empty
 		if(runs != '' && runs != null){
 			for(var i=0; i<runs.length; i++){
-				$('#stats').append('<li class="ui-body-inherit ui-li-static"><strong>Date:</strong>'+runs[i]["date"]+' <br><strong>Distance:</strong>'+runs[i]["miles"]+'m<div class="controls"><a href="#edit" id="editLink" data-miles="'+runs[i]["miles"]+'" data-date="'+runs[i]["date"]+'">Edit</a> | <a href="#">Delete</a></div></li>');
+				$('#stats').append('<li class="ui-body-inherit ui-li-static"><strong>Date:</strong>'+runs[i]["date"]+' <br><strong>Distance:</strong>'+runs[i]["miles"]+'m<div class="controls"><a href="#edit" id="editLink" data-miles="'+runs[i]["miles"]+'" data-date="'+runs[i]["date"]+'">Edit</a> | <a href="#" id="deleteLink" data-miles="'+runs[i]["miles"]+'" data-date="'+runs[i]["date"]+'" onclick ="return confirm(\'Are you sure?\')">Delete</a></div></li>');
 			}
 			$('#home').bind('pageinit', function(){
 				$('#stats').listview('refresh');
 			});
+		}else{
+			$('#stats').html('<p>You have no logged runs</p>');	
 		}
 	 }
 	 
@@ -102,7 +110,45 @@ $(document).one('pageinit', function(){
 		 
 		 return false;
 	 }
-	 	 
+
+	/*
+	 *  Clear Runs
+	 */	 
+	 function clearRuns() {
+		localStorage.removeItem('runs');
+		$('#stats').html('<p>You have no logged runs</p>');
+	 }
+
+	/*
+	 *  Delete Run
+	 */
+	 function deleteRun() {
+		// Set ls items
+		localStorage.setItem('currentMiles',$(this).data('miles'));
+		localStorage.setItem('currentDate',$(this).data('date'));	
+		
+		 // Get current data
+		 var currentMiles = localStorage.getItem('currentMiles');
+		 var currentDate = localStorage.getItem('currentDate');
+
+		 var runs = getRunsObject();
+		 
+		 // Loop through runs (To remove the existing data and make it a string back)
+		 for(var i=0; i<runs.length; i++){
+			if(runs[i].miles == currentMiles && runs[i].date == currentDate){
+				runs.splice(i,1);
+			}
+			localStorage.setItem('runs',JSON.stringify(runs));
+		 }
+		 
+		 alert('Run Deleted');
+		 
+		 //Redirect
+		 window.location.href="index.html";
+		 
+		 return false;
+	 }
+	 	 	 
 	/*
 	 *  Get the runs object
 	 */
